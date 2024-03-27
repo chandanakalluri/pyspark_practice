0437e8f4d0d9e838d549541867415ec79820b7ce
chandanakalluri <kalluri.chandana@diggibyte.com>
2024-03-27 22:17:26 +0530

    reading csv file

--- dataframe/read.py
+++ dataframe/read.py
@@ -4,4 +4,4 @@ spark = SparkSession.builder.appName("reading").getOrCreate()
 df = spark.read.csv(r"C:\Users\Suresh\Documents\ch\detail.csv",header=True,inferSchema=True)
 print(df.show())
 print(df.columns)
-print(df.take(2))
+print(df.take(2))#
