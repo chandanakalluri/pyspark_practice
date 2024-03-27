3cf8eda9d5af21a28b7766c0f55fa2c294e07b35
chandanakalluri <kalluri.chandana@diggibyte.com>
2024-03-27 17:22:00 +0530

    creating rdd

--- RDD/creating rdd.py
+++ RDD/creating rdd.py
@@ -3,4 +3,4 @@ spark=SparkSession.builder.appName("creation of data").getOrCreate()
 data=([1,3,4,5,6,7,4,5,6,7,4,3,3])
 sc=spark.sparkContext
 file1=sc.textFile(r"C:\Users\Suresh\Desktop\New Text Document.txt")
-print(file1.collect())
+print(file1.collect())#
