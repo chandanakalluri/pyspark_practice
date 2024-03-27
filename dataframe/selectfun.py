96505e81b496b08a6f7b86eb46b8c67121114b98
chandanakalluri <kalluri.chandana@diggibyte.com>
2024-03-28 03:02:29 +0530

    select function

---
+++ dataframe/selectfun.py
@@ -0,0 +1,10 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark=SparkSession.builder.appName("selectfun").getOrCreate()
+df=spark.read.csv(r"C:\Users\Suresh\Documents\ch\data.csv",header=True,inferSchema=True)
+df.select('customer','modelproduct').show()
+df.select(df.customer,df.modelproduct).show()
+df.select(df["customer"],df["modelproduct"])
+df.select (col("customer"),col("modelproduct")).show()
+df.select (*df.columns).show()
