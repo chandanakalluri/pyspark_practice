0d648d35623cb93702601631e10ca3e50cb764a0
chandanakalluri <kalluri.chandana@diggibyte.com>
2024-04-02 12:17:57 +0530

    date and time functions are commited

--- dataframe/datefun.py
+++ dataframe/datefun.py
@@ -2,17 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
 spark = SparkSession.builder.appName("date functions").getOrCreate()
-options = {'header': 'true',
-           'inferSchema': 'true',
-           'delimiter': ','}
-path = r"C:\Users\Suresh\Documents\ch\data.csv"
-
-
-def read_file(format_type, options, path):
-    return spark.read.format(format_type).options(**options).load(path)
-
-
-read_df = read_file("csv", options, path)
+read_df = spark.read.csv(r"C:\Users\Suresh\Documents\ch\data1.csv", inferSchema=True, header=True)
 read_df.show()
 
 df = read_df.withColumn("date", current_date())
@@ -20,10 +10,16 @@ df.show()
 c_date = read_df.withColumn("timestamo", current_timestamp())
 c_date.show()
 # add_date=read_df.withColumn("add",add_date("date",7))
+# to=read_df.withColumn("adding",to_date("date"))
+# to.show()
 
-year = read_df.withColumn("Year", year("date"))
-year.show()
-day = read_df.withColumn("Day", day("date"))
-day.show()
-month = read_df.withColumn("Month", month("date"))
-month.show()
+read_df = read_df.withColumn("date", to_date(read_df["date"], "MM-dd-yyyy"))
+
+read_df.printSchema()
+
+year1 = read_df.withColumn("Year", year(read_df.date))
+year1.show()
+day1 = read_df.withColumn("Day", day("date"))
+day1.show()
+month1 = read_df.withColumn("Month", month("date"))
+month1.show()
